5b7f3aaf34fc8bc18f06253870a1ecd9d3c1eda9
Jonatas William <jonatas_2602@hotmail.com>
2019-08-04 09:26:40 -0300

    Bullet

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -1,14 +1,17 @@
 extends Area2D
 
 var dir = Vector2(0, -1) setget set_dir
+onready var init_pos = global_position
 var velocity = 400
 
 func _ready():
+	print(init_pos)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	translate(dir * velocity * delta)
+	
 
 func _on_notifier_screen_exited():	
 	queue_free() # Coloca na fila para limpar a memoria destruindo este item	
